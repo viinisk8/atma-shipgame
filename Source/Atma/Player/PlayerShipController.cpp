96b5b7e57c41ed5b0f21ca5a8e0f5629fc078af1
Vinicius Ferreira <viniciusteologia@gmail.com>
2024-11-11 20:45:49 -0300

    commiting ship rotating with the mouse

--- Source/Atma/Player/PlayerShipController.cpp
+++ Source/Atma/Player/PlayerShipController.cpp
@@ -18,6 +18,15 @@
 #include "PlayerShipController.h"
 
 //     -------------------------------------------------------------------------------------------
+void APlayerShipController::BeginPlay() {
+
+  Super::BeginPlay();
+
+  //
+  CreatePlayerShipReference();
+}
+
+//     -------------------------------------------------------------------------------------------
 void APlayerShipController::AddMovementY(float Value) {
 
   if(APawn* ControlledPawn = GetPawn()) {
@@ -28,8 +37,43 @@ void APlayerShipController::AddMovementY(float Value) {
 //     -------------------------------------------------------------------------------------------
 void APlayerShipController::AddMovementX(float Value) {
 
-  if(APawn* ControlledPawn = GetPawn()) {
+  if (APawn *ControlledPawn = GetPawn()) {
     ControlledPawn->AddMovementInput(FVector(1.0f, 0.0f, 0.0f), Value);
+  }
+}
+
+//     -------------------------------------------------------------------------------------------
+void APlayerShipController::RotateShip() {
+
+  if (PlayerShip && PlayerShip->SkeletalMeshComponent)
+  {
+    FVector MousePosition3D;
+    FVector MouseDirection;
+    
+    if (DeprojectMousePositionToWorld(MousePosition3D, MouseDirection))
+    {
+      
+      FVector ShipLocation = PlayerShip->GetActorLocation();
+      float DirectionX = MousePosition3D.X - ShipLocation.X;
+      
+      FRotator TargetRotation = (DirectionX > 0) ? FRotator(0.0f, 0.0f, 0.0f) : FRotator(0.0f, -180.0f, 0.0f);
+      
+      FRotator CurrentRotation = PlayerShip->SkeletalMeshComponent->GetRelativeRotation();
+      FRotator NewRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, GetWorld()->GetDeltaSeconds(), 5.0f);
+      
+      PlayerShip->SkeletalMeshComponent->SetRelativeRotation(NewRotation);
+    }
+  }
+}
+
+//     -------------------------------------------------------------------------------------------
+void APlayerShipController::CreatePlayerShipReference() {
+
+  APawn *ControlledPawn = GetPawn();
+  if (ControlledPawn && ControlledPawn->IsA(APlayerShip::StaticClass())) {
+    PlayerShip = Cast<APlayerShip>(ControlledPawn);
+  } else {
+    PlayerShip = nullptr;
   }
 }
 
--- Source/Atma/Player/PlayerShipController.h
+++ Source/Atma/Player/PlayerShipController.h
@@ -16,8 +16,10 @@
 
 #pragma once
 
+#include "Components/SkeletalMeshComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "PlayerShip.h"
 //
 #include "PlayerShipController.generated.h"
 
@@ -27,13 +29,24 @@ class ATMA_API APlayerShipController : public APlayerController
 {
 	GENERATED_BODY()
 
+public:
 	// methods
 	UFUNCTION(BlueprintCallable, Category = "Movement")
 	void AddMovementY(float Value);
 
 	UFUNCTION(BlueprintCallable, Category = "Movement")
 	void AddMovementX(float Value);
-	
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	void RotateShip();
+
+	void CreatePlayerShipReference();
 	// Properties
+
+	UPROPERTY(VisibleAnywhere, Category = "Reference")
+	APlayerShip* PlayerShip;
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
